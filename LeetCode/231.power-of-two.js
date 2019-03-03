0f69a839c72eb082c6814e3b82021ba0af21979c
shaw <gggshaw007@gmail.com>
2019-03-03 21:12:12 +0800

    leetcode

---
+++ LeetCode/231.power-of-two.js
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode id=231 lang=javascript
+ *
+ * [231] Power of Two
+ *
+ * https://leetcode.com/problems/power-of-two/description/
+ *
+ * algorithms
+ * Easy (41.56%)
+ * Total Accepted:    212.6K
+ * Total Submissions: 510.9K
+ * Testcase Example:  '1'
+ *
+ * Given an integer, write a function to determine if it is a power of two.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 1
+ * Output: true 
+ * Explanation: 20 = 1
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: 16
+ * Output: true
+ * Explanation: 24 = 16
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: 218
+ * Output: false
+ * 
+ */
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function(n) {
+    
+};
+
